07a0c6f55f593e692c05b52317d1778420ca5f41
imovie23 <54276075+imovie23@users.noreply.github.com>
2019-12-04 11:37:11 +0200

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,13 +4,7 @@ importScripts('https://www.gstatic.com/firebasejs/7.5.0/firebase-messaging.js')
 
 const config = {
 	// Project Settings => Add Firebase to your web app
-	apiKey: 'AIzaSyDFTW69xyMLP3MqvIpDGJyTRO4V0F_OxBg',
-	authDomain: 'admin-pane-f1.firebaseapp.com',
-	databaseURL: 'https://admin-pane-f1.firebaseio.com',
-	projectId: 'admin-pane-f1',
-	storageBucket: 'admin-pane-f1.appspot.com',
-	messagingSenderId: '358924836770',
-	appId: '1:358924836770:web:4cc16972594167eb9a1cfb',
+	
 }
 
 firebase.initializeApp(config)
