778a6592da1d4d36e65c5e46c619e787c5ec2995
Misha Starodubets <starodubetsm@gmail.com>
2020-01-20 18:28:20 +0200

    Some tests

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,7 +15,7 @@ import ListAltIcon from '@material-ui/icons/ListAlt'
 import MenuItem from '@material-ui/core/MenuItem'
 import { Link } from 'react-router-dom'
 import Tooltip from '@material-ui/core/Tooltip'
-import AuthService from "../../services/AuthService"
+import AuthService from '../../services/AuthService'
 
 function HideOnScroll(props) {
 	const { children, window } = props
@@ -45,7 +45,7 @@ export default function HideAppBar(props) {
 		setAnchorEl(null)
 	}
 
-	const location= window.location.pathname
+	const location = window.location.pathname
 
 	return (
 		<React.Fragment>
@@ -82,18 +82,31 @@ export default function HideAppBar(props) {
 								}}
 								open={open}
 								onClose={handleClose}
-							>
-								<Link className={classes.headerLink} to={'/admin-panel'}><MenuItem
-									onClick={handleClose}>Account</MenuItem></Link>
-								<Link className={classes.headerLink} to={'/login'}><MenuItem
-									onClick={()=>{new AuthService().logOut(); handleClose()}}>Logout</MenuItem></Link>
+							><Link className={classes.headerLink} to={'/login'}><MenuItem
+								onClick={() => {
+									new AuthService().logOut()
+									handleClose()
+								}}>Logout</MenuItem></Link>
 								<Link className={classes.headerLink} to={'/createUser'}><MenuItem onClick={handleClose}>Creat
 									User</MenuItem></Link>
 							</CssMenuAdmin>
 						</div>
-						{location==='/kitchen'?(
-							<Link className={classes.headerLink} to={'/admin-panel'}>
-								<Tooltip title="Go to admin-page" aria-label="go to admin-page">
+						{location === '/kitchen' ? (
+								<Link className={classes.headerLink} to={'/admin-panel'}>
+									<Tooltip title="Go to admin-page" aria-label="go to admin-page">
+										<TableIconButton
+											aria-label="account of current user"
+											aria-controls="menu-appbar"
+											aria-haspopup="true"
+											onClick={handleMenu}
+											color="inherit"
+										>
+											<BusinessCenterIcon/>
+										</TableIconButton>
+									</Tooltip>
+								</Link>) :
+							(<Link className={classes.headerLink} to={'/kitchen'}>
+								<Tooltip title="Go to cook" aria-label="go to cook">
 									<TableIconButton
 										aria-label="account of current user"
 										aria-controls="menu-appbar"
@@ -101,23 +114,10 @@ export default function HideAppBar(props) {
 										onClick={handleMenu}
 										color="inherit"
 									>
-										<BusinessCenterIcon/>
+										<RestaurantMenuIcon/>
 									</TableIconButton>
 								</Tooltip>
-							</Link>):
-							(<Link className={classes.headerLink} to={'/kitchen'}>
-							<Tooltip title="Go to cook" aria-label="go to cook">
-								<TableIconButton
-									aria-label="account of current user"
-									aria-controls="menu-appbar"
-									aria-haspopup="true"
-									onClick={handleMenu}
-									color="inherit"
-								>
-									<RestaurantMenuIcon/>
-								</TableIconButton>
-							</Tooltip>
-						</Link>)}
+							</Link>)}
 						<TableIconButton
 							aria-label="account of current user"
 							aria-controls="menu-appbar"
